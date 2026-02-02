7e9cfb13f30e4d87282bdefffa09cdde1d791909
Joe Küng <joekueng05@gmail.com>
2026-02-02 19:46:11 +0100

    fix back-end

--- backend/src/main/java/com/printcalculator/service/ProfileManager.java
+++ backend/src/main/java/com/printcalculator/service/ProfileManager.java
@@ -6,7 +6,6 @@ import com.fasterxml.jackson.databind.node.ObjectNode;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 
-import java.io.File;
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
